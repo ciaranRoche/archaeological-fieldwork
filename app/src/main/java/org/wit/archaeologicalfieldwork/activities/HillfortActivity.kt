a1b6b7356e3bc4ca878b6a9f68e9d173d212910d
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-10-10 12:38:49 +0100

    Add basic hillfort model

--- app/src/main/java/org/wit/archaeologicalfieldwork/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/activities/HillfortActivity.kt
@@ -2,17 +2,33 @@ package org.wit.archaeologicalfieldwork.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.wit.archaeologicalfieldwork.R
 import org.wit.archaeologicalfieldwork.main.MainApp
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.archaeologicalfieldwork.models.HillfortModel
 
-class HillfortActivity : AppCompatActivity() {
+class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     lateinit var app: MainApp
+
+    var hillfort = HillfortModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
 
         app = application as MainApp
+
+        btnAdd.setOnClickListener(){
+            hillfort.name = hillfortName.text.toString()
+            if(hillfort.name.isNotEmpty()){
+                info("add Button Pressed : $hillfort")
+            }else{
+                toast("Please Enter a title")
+            }
+        }
     }
 }
---
+++ app/src/main/java/org/wit/archaeologicalfieldwork/models/HillfortModel.kt
@@ -0,0 +1,3 @@
+package org.wit.archaeologicalfieldwork.models
+
+data class HillfortModel(var name: String = "")
