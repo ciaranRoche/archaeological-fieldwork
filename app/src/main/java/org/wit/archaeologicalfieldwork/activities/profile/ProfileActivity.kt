f3126d7dd66d39f5750d06cc791262e3d86d9bc4
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-10-19 10:44:17 +0100

    Add creation of user

--- app/src/main/java/org/wit/archaeologicalfieldwork/activities/profile/ProfileActivity.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/activities/profile/ProfileActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import kotlinx.android.synthetic.main.activity_profile.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.toast
 import org.wit.archaeologicalfieldwork.R
+import org.wit.archaeologicalfieldwork.main.MainApp
+import org.wit.archaeologicalfieldwork.models.UserModel
 
 class ProfileActivity : AppCompatActivity(), AnkoLogger {
+
+  lateinit var app : MainApp
+  var user = UserModel()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     setTheme(R.style.AppTheme)
@@ -19,6 +25,19 @@ class ProfileActivity : AppCompatActivity(), AnkoLogger {
     setSupportActionBar(toolbarProfile)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+    app = application as MainApp
 
+    saveUser.setOnClickListener {
+      user.name = userName.text.toString()
+      user.email = userEmail.text.toString()
+      user.password = userPassword.text.toString()
+      if(user.email.isNotEmpty() and user.password.isNotEmpty()){
+        app.users.create(user.copy())
+        setResult(AppCompatActivity.RESULT_OK)
+        finish()
+      }else{
+        toast("Please fill out email and password")
+      }
+    }
   }
 }
--- app/src/main/java/org/wit/archaeologicalfieldwork/main/MainApp.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/main/MainApp.kt
@@ -5,14 +5,18 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.wit.archaeologicalfieldwork.models.HillfortJSONStore
 import org.wit.archaeologicalfieldwork.models.HillfortStore
+import org.wit.archaeologicalfieldwork.models.UserJSONStore
+import org.wit.archaeologicalfieldwork.models.UserStore
 
 class MainApp : Application(), AnkoLogger {
 
   lateinit var hillforts: HillfortStore
+  lateinit var users: UserStore
 
   override fun onCreate() {
     super.onCreate()
     hillforts = HillfortJSONStore(applicationContext)
+    users = UserJSONStore(applicationContext)
     info("HillFort Application Started")
   }
 }
