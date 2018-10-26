17e104290d552478b64a81f34d57460faafe65e8
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-10-26 10:58:25 +0100

    Add logout button

--- app/src/main/java/org/wit/archaeologicalfieldwork/activities/profile/ProfileActivity.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/activities/profile/ProfileActivity.kt
@@ -7,7 +7,9 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_profile.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.startActivityForResult
 import org.wit.archaeologicalfieldwork.R
+import org.wit.archaeologicalfieldwork.activities.HillfortListActivity
 import org.wit.archaeologicalfieldwork.main.MainApp
 import org.wit.archaeologicalfieldwork.models.UserModel
 
@@ -47,7 +49,13 @@ class ProfileActivity : AppCompatActivity(), AnkoLogger {
   override fun onOptionsItemSelected(item: MenuItem?): Boolean {
     when (item?.itemId) {
       R.id.item_settings -> startActivityForResult(intentFor<ProfileSettingsActivity>().putExtra("edit_user", loggeduser), 0)
+      R.id.item_logout -> logoutUser()
     }
     return super.onOptionsItemSelected(item)
+  }
+
+  private fun logoutUser() {
+    userLogged = false
+    startActivityForResult<HillfortListActivity>(0)
   }
 }
