0e1d8101f433806f50378dd66dadec6e4348bd68
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-10-27 15:27:15 +0100

    Add hillfort profile toolbar

--- app/src/main/java/org/wit/archaeologicalfieldwork/activities/HillFortProfileActivity.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/activities/HillFortProfileActivity.kt
@@ -3,7 +3,10 @@ package org.wit.archaeologicalfieldwork.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_hill_fort_profile.*
+import org.jetbrains.anko.intentFor
 import org.wit.archaeologicalfieldwork.R
 import org.wit.archaeologicalfieldwork.main.MainApp
 import org.wit.archaeologicalfieldwork.models.HillfortModel
@@ -36,5 +39,17 @@ class HillFortProfileActivity : AppCompatActivity() {
       imageRecycler.layoutManager = LinearLayoutManager(this)
       imageRecycler.adapter = ImageAdapter(hillfort.images)
     }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_hillfort_profile, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId){
+      R.id.item_edit -> startActivityForResult(intentFor<HillfortActivity>().putExtra("hillfort_edit", hillfort), 0)
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
