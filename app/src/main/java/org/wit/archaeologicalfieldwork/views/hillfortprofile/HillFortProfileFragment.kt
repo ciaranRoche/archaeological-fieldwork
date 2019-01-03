ef068945858d6b843fb4a3dc18b8cc3cae8d01dd
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-01-03 12:03:31 +0000

    add initial share function

--- app/src/main/java/org/wit/archaeologicalfieldwork/views/hillfortprofile/HillFortProfileFragment.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/views/hillfortprofile/HillFortProfileFragment.kt
@@ -6,6 +6,7 @@ import androidx.viewpager.widget.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.RatingBar
 import android.widget.TextView
 import kotlinx.coroutines.experimental.android.UI
@@ -15,18 +16,21 @@ import org.wit.archaeologicalfieldwork.R
 import org.wit.archaeologicalfieldwork.adapters.ViewPagerAdapter
 import org.wit.archaeologicalfieldwork.models.data.DataFireStore
 import org.wit.archaeologicalfieldwork.models.data.DataModel
+import android.content.Intent
 
 class HillFortProfileFragment : Fragment(), AnkoLogger {
     lateinit var pagerAdapter: ViewPagerAdapter
     lateinit var ratingBar: RatingBar
     lateinit var hillfort: DataModel
     lateinit var fireStore: DataFireStore
+    lateinit var shareBtn: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_hill_fort_profile, container, false)
         val nameTextview = view.findViewById<TextView>(R.id.hillfortProfileName)
         val descriptionTextView = view.findViewById<TextView>(R.id.hillfortProfileDescription)
         val imagePager = view.findViewById<ViewPager>(R.id.view_pager)
+        shareBtn = view.findViewById(R.id.shareHillfort)
         ratingBar = view.findViewById(R.id.ratingBar)
 
         fireStore = DataFireStore(context!!)
@@ -40,6 +44,15 @@ class HillFortProfileFragment : Fragment(), AnkoLogger {
         ratingBar.rating = hillfort.rating
         pagerAdapter = ViewPagerAdapter(hillfort.images)
         imagePager.adapter = pagerAdapter
+
+        shareBtn.setOnClickListener {
+            val sharingIntent = Intent(android.content.Intent.ACTION_SEND)
+            sharingIntent.type = "text/plain"
+            val shareBody = "Hey I found this awesome hillfort here, lat : ${hillfort.location.lat} , lng : ${hillfort.location.lng}"
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "I found this cool hillfort")
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody)
+            startActivity(Intent.createChooser(sharingIntent, "Share via"))
+        }
 
         return view
     }
