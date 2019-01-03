494293ee78342e3594747c1fd4b7a9afbee2e57b
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-01-03 11:04:43 +0000

    Add Firebase SignOut

--- app/src/main/java/org/wit/archaeologicalfieldwork/views/home/HomePresenter.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/views/home/HomePresenter.kt
@@ -2,6 +2,7 @@ package org.wit.archaeologicalfieldwork.views.home
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.firebase.auth.FirebaseAuth
 import org.jetbrains.anko.startActivity
 import org.wit.archaeologicalfieldwork.R
 import org.wit.archaeologicalfieldwork.views.startup.StartUpView
@@ -11,6 +12,7 @@ class HomePresenter(val view: HomeView) {
 
     fun doLogout() {
         userLogged = false
+        FirebaseAuth.getInstance().signOut()
         view.startActivity<StartUpView>()
     }
 
